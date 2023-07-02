c6536e2b1e9e7aa7cf2662a6982a73bf58fe7288
Bayali12 <zloo095@yandex.ru>
2023-07-02 17:24:55 +0300

    change api_url

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -1,2 +1,3 @@
-const API_URL = 'http://localhost:8081/api/todo';
+// const API_URL = 'http://localhost:8081/api/todo';
+const API_URL = 'https://todushka-backend.onrender.com/api/todo';
 export default API_URL;
